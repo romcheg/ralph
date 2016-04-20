620142267ebd1b58f7b44334cc5d1cd96768cb6b
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-04-20 09:55:35 +0200

    USE_TZ controlled by env variable

--- src/ralph/settings/base.py
+++ src/ralph/settings/base.py
@@ -157,7 +157,7 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
 TIME_ZONE = os.getenv('TIME_ZONE', 'Europe/Warsaw')
 USE_I18N = bool_from_env('USE_I18N', True)
 USE_L10N = bool_from_env('USE_L10N', True)
-USE_TZ = True
+USE_TZ = os_env_true('USE_TZ', 'False')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
